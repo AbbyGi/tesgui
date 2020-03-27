31a90e2238bd56c1c895776ca1bec108bcf0c1b3
AbbyGiles <agiles@student.sjcny.edu>
2020-03-27 15:14:25 -0400

    Got main screen to show each section properly

--- tesgui/main.py
+++ tesgui/main.py
@@ -1,28 +1,33 @@
 import os
 import sys
 import json
-from pydm import Display
-# from qtpy.QtWidgets import (QVBoxLayout, QHBoxLayout, QGroupBox,
-#     QLabel, QLineEdit, QPushButton, QScrollArea, QFrame,
-#     QApplication, QWidget)
 
 from pydm.widgets import PyDMEmbeddedDisplay
 
 from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton)
 from PyQt5 import uic
 
-form, base = uic.loadUiType('MainWindow.ui')
+import time
+from os import path
+from pydm import Display
 
 
-class MainWidget(base, form):
+class MainScreen(Display):
+    def __init__(self, parent=None, args=None):
+        super(MainScreen, self).__init__(parent=parent, args=args)
+        self.ui.PyDMEmbeddedDisplay.filename = 'motor_params.py'
+        # # Attach our custom process_image method
+        # self.ui.imageView.process_image = self.process_image
+        # # Hook up to the newImageSignal so we can update
+        # # our widgets after the new image is done
+        # self.ui.imageView.newImageSignal.connect(self.show_blob)
+        # # Store blob coordinate
+        # self.blob = (0, 0)
 
-    def __init__(self):
-        super(base, self).__init__()
-        self.setupUi(self)
+    def ui_filename(self):
+        # Point to our UI file
+        return 'main.ui'
 
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    w = MainWidget()
-    w.show()
-    sys.exit(app.exec_())
+    def ui_filepath(self):
+        # Return the full path to the UI file
+        return path.join(path.dirname(path.realpath(__file__)), self.ui_filename())
--- tesgui/motor_params.py
+++ tesgui/motor_params.py
@@ -1,10 +1,69 @@
-import os
 import json
 from pydm import Display
-# from qtpy.QtWidgets import (QVBoxLayout, QHBoxLayout, QGroupBox,
-#     QLabel, QLineEdit, QPushButton, QScrollArea, QFrame,
-#     QApplication, QWidget)
-
+from qtpy.QtWidgets import (QVBoxLayout, QHBoxLayout, QGroupBox,
+                            QLabel, QLineEdit, QPushButton, QScrollArea, QFrame,
+                            QApplication, QWidget)
+from qtpy import QtCore
 from pydm.widgets import PyDMEmbeddedDisplay
 
-# dynamically creating motor param section
+
+class AllMotors(Display):
+    def __init__(self, parent=None, args=[], macros=None):
+        super(AllMotors, self).__init__(parent=parent, args=args, macros=None)
+        self.app = QApplication.instance()
+        self.results_layout = None
+        self.frm_result = None
+        self.all_motor_layout = None
+        self.motors = []
+        self.load_motors()
+        self.setup_ui()
+        self.show_motors()
+
+    def ui_filepath(self):
+        return None
+
+    def setup_ui(self):
+        main_layout = QVBoxLayout()
+        self.setLayout(main_layout)
+        lbl_title = QLabel("Motor Parameters")
+        lbl_title.setStyleSheet("\
+                    QLabel {\
+                        qproperty-alignment: AlignCenter;\
+                        padding: 5px 0px;\
+                        max-height: 25px;\
+                        font-size: 14px;\
+                    }")
+        main_layout.addWidget(lbl_title)
+        self.results_layout = QVBoxLayout()
+        self.results_layout.setContentsMargins(0, 0, 0, 0)
+
+        # Create a Frame to host the results of search
+        self.frm_result = QFrame(parent=self)
+        self.frm_result.setLayout(self.results_layout)
+
+        # Create a ScrollArea so we can properly handle
+        # many entries
+        scroll_area = QScrollArea(parent=self)
+        scroll_area.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOn)
+        scroll_area.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        scroll_area.setWidgetResizable(True)
+
+        # Add the Frame to the scroll area
+        scroll_area.setWidget(self.frm_result)
+
+        # Add the scroll area to the main layout
+        main_layout.addWidget(scroll_area)
+
+    def load_motors(self):
+        self.motors = ['sample_stage.x', 'sample_stage.y', 'sample_stage.z']
+
+    def show_motors(self):
+        for m in self.motors:
+            disp = PyDMEmbeddedDisplay(parent=self)
+            disp.macros = json.dumps({"MOTOR": m})
+            disp.filename = 'motors.ui'
+            disp.setMinimumWidth(300)
+            disp.setMinimumHeight(20)
+            disp.setMaximumHeight(30)
+            # Add the Embedded Display to the Results Layout
+            self.results_layout.addWidget(disp)
